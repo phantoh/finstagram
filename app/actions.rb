ac23d490de7bc05123f4b3d267ca5d05b8e8ea8f
phantoh <phanp@phanp.com>
2021-03-08 00:24:09 +0000

    W2D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,52 @@
-get '/' do
-    File.read(File.join('app/views', 'index.html'))
+
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes >= 60
+        "#{time_ago_in_minutes / 60} hours ago"
+    else
+        "#{time_ago_in_minutes} minutes ago"
+    end
 end
+
+get '/' do
+    finstigram_post_shark = {
+        username: "sharky_j",
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naser.com/images/shark.jpg",
+        humanized_time_ago: humanized_time_ago(15),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            usernam: "sharky_j",
+            text: "Out for the long weekend... too embarassed to show y'all the beach bod!"
+        }]     
+    }
+
+    finstigram_post_whale = {
+        username: "kirk_whalum",
+        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+        photo_url: "http://naserca.com/images/whale.jpg",
+        humanized_time_ago: humanized_time_ago(65),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "kirk_whalum",
+            text: "#weekendvibes"
+        }]
+    }
+
+    finstigram_post_marlin = {
+        username: "marlin_peppa",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        photo_url: "http://naserca.com/images/marlin.jpg",
+        humanized_time_ago: humanized_time_ago(190),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "marlin_peppa",
+            text: "lunchtime! ;)"
+        }]
+    }
+
+    [finstigram_post_shark, finstigram_post_whale, finstigram_post_marlin].to_s
+end
+
